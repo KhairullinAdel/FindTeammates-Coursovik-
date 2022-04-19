4b003f7642a867ff834706f5528fb300b3f5d1e9
h4irullin.adel@yandex.ru <h4irullin.adel@yandex.ru>
2022-04-20 00:03:27 +0300

    feat: Connection application to Firebase by Firesharp

---
+++ Finder.Core/FireBase/FBaseConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace Finder_Core.FireBase
+{
+    class FBaseConfig
+    {
+        IFirebaseConfig config = new FirebaseConfig()
+        {
+            AuthSecret = "Oq74Wtm31h2KJQv4fqHgwIQTt7jhaODV4tDKc3eS",
+            BasePath = "https://findteammateapplication-default-rtdb.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+    }
+}
